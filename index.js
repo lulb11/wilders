7c36f36008071ddaa53230a52b709946b0a2ce06
lulb11 <bordeslucas@gmail.com>
2023-10-03 10:46:26 +0200

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+
+function printName() { 
+    console.log(chalk.blue("Kevin"));
+    console.log(chalk.red("Jean Kevin"));
+    console.log(chalk.green("Jean Dylan"));
+    console.log(chalk.yellow("Dylan"));
+};
+
+printName();
